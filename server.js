66768ea2d1606171392fcbe94a6333dbc9430f82
yurenadalmera-dot <yurena.dalmera@gmail.com>
2026-08-25 12:10:43 +0100

    Serve index.html from root instead of public directory

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 );
 app.listen(process.env.PORT || 3000);
